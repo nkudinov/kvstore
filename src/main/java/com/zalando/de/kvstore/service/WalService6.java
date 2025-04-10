75e9994069329f12476fadf7124e9511b04b4284
Nikolay Kudinov <nikolay.a.kudinov@gmail.com>
2025-04-10 12:21:09 +0200

    one new version

--- src/main/java/com/zalando/de/kvstore/service/WalService6.java
+++ src/main/java/com/zalando/de/kvstore/service/WalService6.java
@@ -14,27 +14,80 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 import java.util.zip.CRC32;
 
 public class WalService6 implements WALInterface {
 
+    static class WALRecord {
+
+        private String key;
+        private String val;
+
+        final CompletableFuture<Void> future = new CompletableFuture<>();
+
+        public WALRecord(String key, String val) {
+            this.key = key;
+            this.val = val;
+        }
+    }
 
     public static final int MAGIC_NUMBER = 0xBEBEBEBE;
     Lock lock;
     RandomAccessFile raf;
 
+    BlockingQueue<WALRecord> queue;
+    final Thread worker;
+    private final AtomicBoolean isRunning;
+
     public WalService6() throws IOException {
         lock = new ReentrantLock();
         raf = new RandomAccessFile(WAL_LOG, "rw");
         raf.seek(raf.length());
+        queue = new LinkedBlockingQueue<>();
+        isRunning = new AtomicBoolean(true);
+        worker = new Thread() {
+            @Override
+            public void run() {
+                while (isRunning.get()) {
+                    WALRecord walRecord = null;
+                    try {
+                        walRecord = queue.take();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    try {
+                        writeToDisk(walRecord.key, walRecord.val);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                    walRecord.future.complete(null);
+                }
+            }
+        };
+        worker.start();
     }
 
     public static final String WAL_LOG = "wal.log";
 
     @Override
     public void write(String key, String val) throws IOException {
+        WALRecord walRecord = new WALRecord(key, val);
+        queue.offer(walRecord); // enqueue for background writing
+        try {
+            walRecord.future.join(); // wait until it's written
+        } catch (Exception e) {
+            throw new IOException("WAL write failed", e);
+        }
+    }
+
+
+    private void writeToDisk(String key, String val) throws IOException {
         lock.lock();
         try {
             try (ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
@@ -60,7 +113,16 @@ public class WalService6 implements WALInterface {
 
     @Override
     public void shutdown() throws IOException {
-        raf.close();
+        lock.lock();
+        try {
+            isRunning.set(false);
+            worker.interrupt();
+            if (raf != null) {
+                raf.close();
+            }
+        } finally {
+            lock.unlock();
+        }
     }
 
     @Override
